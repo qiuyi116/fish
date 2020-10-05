7a8e959197bef9bb9eb20300c2ae3e2d949ca05d
qiuyi116 <qiuyi116@gmail.com>
2020-10-05 23:37:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# fish
+fish config.
+
+- install fish shell
+- clone this repo to `~/.config/` dir
+- modify `config.fish` files
---
+++ config.fish
@@ -0,0 +1,18 @@
+export EDITOR="vim"
+alias vi='nvim'
+alias vim='nvim'
+alias grep="grep --color=auto"
+alias c=clear
+
+# ssh login to local yqiu-mbp
+alias mbp="ssh QY@192.168.50.99"
+
+export MBP="192.168.50.99"
+alias proxy='export https_proxy=socks5://127.0.0.1:1080'
+alias unproxy='unset https_proxy'
+
+alias gits="git status"
+alias gitd="git diff"
+alias gita="git add ."
+alias gitl="git log"
+alias gitb="git branch"
